d4794f28e2a4e68def11509f923aca1b55ec50e2
Nikola Simić <nikola7simic@gmail.com>
2024-04-02 09:33:22 +0200

    feat: 2.1 2.2 dockerize monolith and database (#5)

---
+++ src/ui/Explorer/src/env/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiHost: 'http://localhost:8080/api/',
+  mapBoxApiKey:
+    'pk.eyJ1IjoicHN3OCIsImEiOiJjbG8xbmJ6MW0xbnliMnJxb2hyd2pvM2I4In0.wvr68_34P6rpWiVXq64NLA',
+};
