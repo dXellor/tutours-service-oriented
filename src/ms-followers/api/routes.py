a3058997ac004cb2ef5acabaaa8a5ce94c62ccc4
Nikola Simic <nikola7simic@gmail.com>
2024-04-21 18:29:54 +0200

    feat: create endpoints for following management

--- src/ms-followers/api/routes.py
+++ src/ms-followers/api/routes.py
@@ -1,5 +1,14 @@
+from dotenv import load_dotenv
 from fastapi import APIRouter
+import os
 
+from service.follower_service import FollowerService
+
+load_dotenv()
+follower_service = FollowerService(os.getenv('DB_NAME'),
+                                    os.getenv('DB_URI'),
+                                    os.getenv('DB_USER'), 
+                                    os.getenv('DB_PWD'))
 router = APIRouter()
 
 @router.get('/', status_code=200)
@@ -9,3 +18,15 @@ async def test_endpoint():
     }
 
     return reponse
+
+@router.post('/follow/{userId1}/{userId2}')
+async def follow(userId1: int, userId2: int):
+    follower_service.follow(userId1, userId2)
+
+@router.get('/followings/{userId}')
+async def get_followings(userId: int):
+    return follower_service.get_followings(userId)
+
+@router.get('/recommendations/{userId}')
+async def get_recommendations(userId: int):
+    return follower_service.get_recommendations(userId)
---
+++ src/ms-followers/service/follower_service.py
@@ -0,0 +1,63 @@
+import logging
+from neo4j import GraphDatabase
+
+class FollowerService():
+
+    def __init__(self, db_name, db_uri, username, password) -> None:
+        self.db_name = db_name
+        self.driver = GraphDatabase.driver(db_uri, auth=(username, password))
+        self.driver.verify_connectivity()
+        self.__init_database()
+
+    def __del__(self) -> None:
+        self.driver.close()
+
+    def __init_database(self):
+        try:
+            self.driver.execute_query("CREATE CONSTRAINT FOR (user:USER) REQUIRE user.id IS UNIQUE;")
+        except:
+            logging.info("Database is already setup")
+
+    def __insertIfNotExist(self, userId: int) -> None:
+        records, _, _ = self.driver.execute_query(
+            "MATCH (u:USER {id: $userId}) RETURN u.id",
+            userId=userId,
+            database_= self.db_name
+        )
+
+        if len(records) == 0:
+            self.driver.execute_query("CREATE (u:USER {id: $userId})", userId=userId)
+
+    def follow(self, signedInUserId: int, userToBeFollowedId: int ) -> None:
+        self.__insertIfNotExist(signedInUserId)
+        self.__insertIfNotExist(userToBeFollowedId)
+
+        self.driver.execute_query(
+            "MATCH (signedInUser:USER {id: $signedInUserId}), (userToBeFollowed:USER{id: $userToBeFollowedId}) CREATE (signedInUser)-[:FOLLOWS]->(userToBeFollowed)",
+            database_= self.db_name,
+            signedInUserId=signedInUserId,
+            userToBeFollowedId=userToBeFollowedId,
+        )
+
+    def get_followings(self, signedInUserId: int):
+        records, summary, keys = self.driver.execute_query(
+            "MATCH (u:USER {id: $userId})-[:FOLLOWS]->(u2:USER) RETURN collect(u2.id)",
+            database_= self.db_name,
+            userId=signedInUserId
+        )
+
+        return records
+    
+    def get_recommendations(self, signedInUserId: int):
+        records, summary, keys = self.driver.execute_query(
+            """MATCH (u1:USER {id: 1})-[:FOLLOWS]->(u2:USER)-[:FOLLOWS]->(u3:USER) 
+            WHERE u1 <> u3 
+            AND NOT (u1)-[:FOLLOWS]->(u3) 
+            RETURN collect(u3.id)""",
+            database_= self.db_name,
+            userId=signedInUserId
+        )
+
+        return records
+
+        
